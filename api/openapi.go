d7e17a9c17c2710754cb83ec1cf0b2dd5de185de
Viky Bukovská <xbukovska@stuba.sk>
2026-05-02 18:36:03 +0200

    added swagger for testing and mongo

--- api/openapi.go
+++ api/openapi.go
@@ -10,6 +10,13 @@ import (
 //go:embed physio.openapi.json
 var openapiSpec []byte
 
+//go:embed swagger.html
+var swaggerUI []byte
+
 func HandleOpenApi(ctx *gin.Context) {
 	ctx.Data(http.StatusOK, "application/json", openapiSpec)
+}
+
+func HandleSwaggerUI(ctx *gin.Context) {
+	ctx.Data(http.StatusOK, "text/html; charset=utf-8", swaggerUI)
 }
--- cmd/api-service/main.go
+++ cmd/api-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"strings"
@@ -9,18 +10,30 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/meowiky/WAC-Physio_Bukovska-Biely-API/api"
+	"github.com/meowiky/WAC-Physio_Bukovska-Biely-API/internal/physio"
 )
 
 func main() {
 	log.Printf("Server started")
-	port := os.Getenv("API_PORT")
+	port := os.Getenv("PHYSIO_API_PORT")
 	if port == "" {
 		port = "8080"
 	}
-	environment := os.Getenv("API_ENVIRONMENT")
+	environment := os.Getenv("PHYSIO_API_ENVIRONMENT")
 	if !strings.EqualFold(environment, "production") { // case insensitive comparison
 		gin.SetMode(gin.DebugMode)
 	}
+
+	server, err := physio.NewServer(context.Background())
+	if err != nil {
+		log.Fatalf("failed to initialize mongo connection: %v", err)
+	}
+	defer func() {
+		if err := server.Disconnect(context.Background()); err != nil {
+			log.Printf("failed to disconnect mongo client: %v", err)
+		}
+	}()
+
 	engine := gin.New()
 	engine.Use(gin.Recovery())
 	corsMiddleware := cors.New(cors.Config{
@@ -33,5 +46,18 @@ func main() {
 	})
 	engine.Use(corsMiddleware)
 	engine.GET("/openapi", api.HandleOpenApi)
-	engine.Run(":" + port)
+	engine.GET("/swagger", api.HandleSwaggerUI)
+
+	physio.NewRouterWithGinEngine(engine, physio.ApiHandleFunctions{
+		AmbulancesAPI:             server,
+		AvailabilityAPI:           server,
+		PatientsAPI:               server,
+		RehabilitationPlansAPI:    server,
+		RehabilitationSessionsAPI: server,
+		TherapistsAPI:             server,
+	})
+
+	if err := engine.Run(":" + port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
